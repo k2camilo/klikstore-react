09f3f1d4c43783bfcd84a07880cf8f39183f1f84
Camilo Gutierrez <k2camilo@gmail.com>
2025-11-28 10:17:37 -0500

    se desconfigura el despliegue

--- src/components/carrusel.js
+++ src/components/carrusel.js
@@ -7,9 +7,9 @@ export default function Carrusel(){
     console.log("✅ Carrusel montado");
 
     const baners = [
-        process.env.PUBLIC_URL + "/img/Banner1.jpg",
-        process.env.PUBLIC_URL + "/img/Banner2.jpg",
-        process.env.PUBLIC_URL + "/img/Banner3.jpg",
+        "/img/Banner1.jpg",
+        "/img/Banner2.jpg",
+        "/img/Banner3.jpg",
     ];
 
     const [index, setIndex] = useState(0);
